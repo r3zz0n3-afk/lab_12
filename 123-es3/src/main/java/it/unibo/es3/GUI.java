b0bd1ba9c829cb91cd96287333f2620e3820854f
Andrea_Ricci <andrea.ricci60@studio.unibo.it>
2025-12-11 01:35:10 +0100

    Start implementation the 123 exercise

--- 123-es3/src/main/java/it/unibo/es3/GUI.java
+++ 123-es3/src/main/java/it/unibo/es3/GUI.java
@@ -3,10 +3,15 @@ package it.unibo.es3;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.io.Serial;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * GUI for the game.
@@ -16,28 +21,45 @@ public final class GUI extends JFrame {
     @Serial
     private static final long serialVersionUID = 1L;
     private final List<JButton> cells = new ArrayList<>();
-
+    private final Logics logics;
+    private Random rnd = new Random();
     /**
      * Constructor.
      *
      * @param width the size of the grid
      */
     public GUI(final int width) {
+        this.logics = new LogicImpl(width);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         // Create a panel with a grid layout
-        final JPanel panel = new JPanel(new GridLayout(width, width));
+        final JPanel panel = new JPanel(new BorderLayout());
+        final JPanel grid = new JPanel(new GridLayout(width, width));
         this.getContentPane().add(panel);
         // Create buttons and add them to the panel
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < width; j++) {
                 final var pos = new Pair<>(j, i);
-                final JButton button = new JButton(pos.toString());
+                final JButton button = new JButton(" ");
                 this.cells.add(button);
-                button.addActionListener(e -> button.setText(String.valueOf(cells.indexOf(button))));
-                panel.add(button);
+                button.addActionListener(e -> button.setText(logics.hit(pos)));
+                grid.add(button);
             }
         }
+        // Mark three distinct random cells with "*"
+        final int total = width * width;
+        final Set<Integer> picks = new HashSet<>();
+        while (picks.size() < 3) {
+            picks.add(this.rnd.nextInt(total));
+        }
+        for (final Integer idx : picks) {
+            this.cells.get(idx).setText("*");
+        }
+        final JButton exspanziButton = new JButton(">");
+        panel.add(exspanziButton, BorderLayout.SOUTH);
+        panel.add(grid, BorderLayout.AFTER_LINE_ENDS);
         pack();
         this.setVisible(true);
     }
+
+
 }
---
+++ 123-es3/src/main/java/it/unibo/es3/LogicImpl.java
@@ -0,0 +1,64 @@
+package it.unibo.es3;
+
+public class LogicImpl implements Logics{
+    private String[][] grid;
+
+    public LogicImpl(final int size) {
+        grid = new String[size][size];
+        setUp();
+    }
+
+    private void setUp() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                grid[i][j] = " ";
+            }
+        }
+    }
+
+    @Override
+    public String hit(Pair<Integer, Integer> elem) {
+        int x = elem.x().intValue();
+        int y = elem.y().intValue();
+        if(grid[x][y] == " " ) {
+            grid[x][y] = "*";
+        } else {
+            grid[x][y] = " ";
+        }
+
+        return grid[x][y] ;
+    }
+
+    @Override
+    public boolean toQuit() {
+        // Controlla se una riga è tutta *
+        for (int i = 0; i < grid.length; i++) {
+            boolean rowAllStars = true;
+            for (int j = 0; j < grid.length; j++) {
+                if (!grid[i][j].equals("*")) {
+                    rowAllStars = false;
+                    break;
+                }
+            }
+            if (rowAllStars) {
+                return true;
+            }
+        }
+        
+        // Controlla se una colonna è tutta *
+        for (int j = 0; j < grid.length; j++) {
+            boolean colAllStars = true;
+            for (int i = 0; i < grid.length; i++) {
+                if (!grid[i][j].equals("*")) {
+                    colAllStars = false;
+                    break;
+                }
+            }
+            if (colAllStars) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
